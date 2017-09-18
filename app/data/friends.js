9818fafd4110021e2398fe34487eec776cf9a570
afletch24 <anfletcher24@gmail.com>
2017-09-18 16:32:56 -0500

    final build

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,23 @@
 console.log("friends.js page has loaded");
 
-var friendsArray = [];
 
-module.exports = friendsArray;
+exports.friendsArray= [
+    {
+         "name": "Ricky Bobby",
+        "photo": "https://scontent-dft4-1.xx.fbcdn.net/v/t1.0-9/18765561_10154806666071871_2452798636540453276_n.jpg?oh=6cae5a1a7955148122810b52683ec89d&oe=5A187AB0",
+        "scores": 
+        [
+            5, 1,4, 4,5, 1, 2,5, 4,1
+        ]
+    },
+    {
+         "name": "Joey Tribbiani",
+        "photo": "https://www.google.com/search?q=joey+tribbiani&tbm=isch&imgil=DbCrQeb1TaoMrM%253A%253BPal5ihgUJL15iM%253Bhttps%25253A%25252F%25252Fwww.youtube.com%25252Fwatch%25253Fv%2525253DdsHzbm39gjQ&source=iu&pf=m&fir=DbCrQeb1TaoMrM%253A%252CPal5ihgUJL15iM%252C_&usg=__jyuR9_iTFqQuSxIT5yywegagse8%3D&biw=1365&bih=835&ved=0ahUKEwjY5re3xq_WAhVl2oMKHe3hDGgQyjcItgE&ei=WSnAWZjWDuW0jwTtw7PABg#imgrc=rIQjiWL6X4GzdM:",
+        "scores": 
+        [
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+        ]
+    }
+];
+
+// module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,8 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-console.log("apiRouates page has loaded");
+console.log("apiRoutes page has loaded");
 var path = require("path");
-var friends= require("/app/data/friends.js");
+var friends= require("../data/friends.js").friendsArray;
 
 
 
@@ -14,33 +14,53 @@ module.exports = function(app){
 
     app.post("/api/friends", function(req, res){
         var userData = req.body;
-        console.log("user input= " + JSON.stringify(userData));
-        var UserScores = userData.scores;
+        // console.log("user input= " + JSON.stringify(userData));
+        var userScores = userData.scores;
 
-        var matchName= "";
-        var matchImage="";
-        var totalDifference = 1000000;
-
-        for(var i=0; i < friends.length; i++){
-            var difference = 0;
-            
-            for(var j =0; j <userScores.length; j++){
-                difference+= Math.abs(friends[i].scores[j] - userScores[j]);
-            }
-            console.log("difference= " + difference);
-
-            if(difference < totalDifference){
-
-                totalDifference = difference;
-                matchName = friends[i].name;
-                matchImage = friends[i].photo;
-            }
+        for(var i=0; i < userScores.length; i++){
+            userScores[i] = parseInt(userScores[i]);
         }
 
-        friends.push(userData);
-       
-    });
+        // userScores = userData.scores.map(score => parseInt(score));
 
+        var match= matchMath(userScores);
+
+        friends.push({
+            name: userData.name,
+            photo: userData.photo,
+            scores: userScores
+        });
+       
+        res.send(match);
+    });
+   
 }
 
 
+function matchMath (userScores){
+    var matchName= "";
+    var matchImage="";
+    var totalDifference = 1000000;
+
+    for(var i=0; i < friends.length; i++){
+        var difference = 0;
+        
+        for(var j =0; j <userScores.length; j++){
+            difference+= Math.abs(friends[i].scores[j] - userScores[j]);
+        }
+        // console.log("difference= " + difference);
+
+        if(difference < totalDifference){
+
+            totalDifference = difference;
+            matchName = friends[i].name;
+            matchImage = friends[i].photo;
+            // look into sort
+        }
+    }
+
+    return {
+        name: matchName,
+        image: matchImage
+    };
+}
